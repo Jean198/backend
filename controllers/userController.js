5b3f361450f5db9d54fe4add245accd58a9414fa
Jean198 <jeandedieu.niyigaba@gmail.com>
2023-02-26 14:09:22 +0200

    fixing bugs

--- controllers/userController.js
+++ controllers/userController.js
@@ -94,7 +94,7 @@ const loginUser = asyncHandler(async (req, res) => {
       httpOnly: true,
       expires: new Date(Date.now() + 1000 * 86400),
       //sameSite: 'strict', // disabling this helped to get the cookie in the browser
-      secure: true,
+      //secure: true,
     });
     const { _id, name, email, photo, phone, bio } = user;
     res.status(200).json({
