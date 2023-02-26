6c5dd280148762fa97e717a4821a5518725dc15d
Jean198 <jeandedieu.niyigaba@gmail.com>
2023-02-26 14:58:25 +0200

    fixing bugs

--- controllers/userController.js
+++ controllers/userController.js
@@ -120,8 +120,8 @@ const logoutUser = asyncHandler(async (req, res) => {
     path: '/',
     httpOnly: true,
     expires: new Date(0),
-    //sameSite: 'none',
-    //secure: true,
+    sameSite: 'none',
+    secure: true,
   });
 
   res.status(200).json({ message: 'User logout succesfully!' });
